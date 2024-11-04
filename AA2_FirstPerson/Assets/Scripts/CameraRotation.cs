8bba47be21a369dd8cd439ef3725748de30416a9
jajo560 <joseantonio.jimenez@enti.cat>
2024-11-04 23:09:12 +0100

    código de pick up y salto

--- AA2_FirstPerson/Assets/Scripts/CameraRotation.cs
+++ AA2_FirstPerson/Assets/Scripts/CameraRotation.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class CameraRotation : MonoBehaviour
 {
+    public GameObject torreta;
     public GameObject troncoTorreta;
     private float rotation_y = 0;
     private float rotation_x = 0;
+    private float rotation_x2 = 0;
+    private float rotation_y2 = 0;
+    public float sensibility_X;
+    public float sensibility_Y;
+    public float maxRotationY = 45.0f;
+    public float minRotationY = -45.0f;
+    public float maxRotationX = 45.0f;
+    public float minRotationX = -45.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,12 +26,34 @@ public class CameraRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rotation_y = Input.GetAxis("Mouse Y");
-        rotation_x = Input.GetAxis("Mouse X");
+        rotation_y = Input.GetAxis("Mouse Y") * sensibility_Y;
+        rotation_x = Input.GetAxis("Mouse X") * sensibility_X;
 
-        troncoTorreta.transform.localEulerAngles = transform.localEulerAngles + new Vector3(-rotation_y, 0, 0);
-        troncoTorreta.transform.localEulerAngles = transform.localEulerAngles + new Vector3(0, rotation_x, 0);
+        rotation_x2 += rotation_x;
 
+        rotation_y2 -= rotation_y;
+
+        if (rotation_y2 > maxRotationY)
+        {
+            rotation_y2 = maxRotationY;
+        }
+        else if (rotation_y2 < minRotationY)
+        {
+            rotation_y2 = minRotationY;
+        }
+
+        if (rotation_x2 > maxRotationX)
+        {
+            rotation_x2 = maxRotationX;
+        }
+        else if (rotation_x2 < minRotationX)
+        {
+            rotation_x2 = minRotationX;
+        }
+
+        torreta.transform.localRotation = Quaternion.Euler(0, rotation_x2, 0);
+        troncoTorreta.transform.localRotation = Quaternion.Euler(rotation_y2, 0, 0);
+        
 
 
     }
---
+++ AA2_FirstPerson/Assets/Scripts/Movement.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;         
+    public float jumpForce = 5f;          
+    public LayerMask groundLayer;         
+    public float raycastLength = 1.1f;    
+
+    private Rigidbody rb;                 
+    private bool isGrounded;              
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        CheckGround();
+
+        if (isGrounded)
+        {
+            MovePlayer();
+            if (Input.GetButtonDown("Jump"))
+            {
+                Jump();
+            }
+        }
+    }
+
+    void CheckGround()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+        RaycastHit hit;
+
+        Debug.DrawRay(transform.position, Vector3.down * raycastLength, Color.red);
+        if (Physics.Raycast(ray, out hit, raycastLength, groundLayer))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    void MovePlayer()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = (transform.forward * moveVertical + transform.right * moveHorizontal).normalized;
+        rb.MovePosition(transform.position + moveDirection * moveSpeed * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+}
